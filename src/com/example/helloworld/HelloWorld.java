6127d09238661e4c5a598a70015fc8504e795807
Wei Chauncey <13166971592@163.com>
2020-11-30 11:00:09 +0800

    initializing repository

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+
+package com.example.helloworld;
+/**
+ * @author weicx
+ * @create 2020-11-12 13:16
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        Object string;
+        System.out.println();
+        System.out.println("args");
+
+    }
+}
